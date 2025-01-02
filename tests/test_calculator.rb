12733de297dd0c51480ddbe2046853bd77d69bd8
Jose Aguirre <josito.aguirre@outlook.com>
2025-01-01 20:52:12 -0600

    subtract test

--- tests/test_calculator.rb
+++ tests/test_calculator.rb
@@ -2,8 +2,8 @@ require 'minitest/autorun'
 require './calculator'
 
 class TestCalculator < Minitest::Test
-  def test_addition
+  def test_subtraction
     calculator = Calculator.new
-    assert_equal 4, calculator.add(2, 2), " Addition method failed"
+    assert_equal 0, calculator.subtract(2, 2), " Subtraction method failed"
   end
 end
